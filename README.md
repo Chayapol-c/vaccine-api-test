087b53ca534bd940b4070d2c8494cafa74662a25
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2021-11-18 15:08:39 +0700

    refactor some tests

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Vaccine API Testing
+
 This repo is the WCG group's API testing based on this [document](https://wcg-apis.herokuapp.com/api-doc/)
 
+## Test case list
+
+| ID  | Testcase                                  | Testcase Description                                      | Status |
+| --- | ----------------------------------------- | --------------------------------------------------------- | ------ |
+| 1   | test_post_registration()                  | create a registration                                     | pass   |
+| 2   | test_post_duplicate_registration()        | create a registration 2 times                             | pass   |
+| 3   | test_post_with_missing_param_citizen_id() | create a registration with missing param citizen id       | pass   |
+| 4   | test_post_with_more_than_13_digits_id()   | create a registration with 13 more digits citizen id      | pass   |
+| 5   | test_post_with_invalid_birth_date()       | create a registration with invalid birth date             | pass   |
+| 6   | test_post_with_d_m_y_birth_date()         | create a registration with dd/mm/yyyy format              | pass   |
+| 7   | test_post_with_y_m_d_birth_date()         | create a registration with yyyy/mm/dd format              | pass   |
+| 8   | test_post_with_m_d_y_birth_date()         | create a registration with mm/dd/yyyy format              | pass   |
+| 8   | test_post_with_m_y_d_birth_date()         | create a registration with mm/yyyy/dd format              | pass   |
+| 8   | test_post_with_d_y_m_birth_date()         | create a registration with dd/yyyy/mm format              | pass   |
+| 9   | test_post_with_less_than_12_years_old()   | create a registration with less than 12 years old citizen | pass   |
+| 10  | test_post_with_future_date()              | create a registration with future birth date              | pass   |
+| 11  | test_remove_invalid_registration()        | remove an invalid registration                            | pass   |
+| 12  | test_remove_registration()                | remove a registration                                     | pass   |
+| 13  | test_get_registration()                   | retrieve a registration                                   | pass   |
+| 14  | test_get_invalid_registration()           | retrieve an invalid registration                          | pass   |
--- test_registration.py
+++ test_registration.py
@@ -1,9 +1,8 @@
 """Test file for WCG api webservice."""
-from typing import cast
 import unittest
 import requests
 from decouple import config
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class RegistrationTest(unittest.TestCase):
@@ -23,18 +22,14 @@ class RegistrationTest(unittest.TestCase):
             "is_risk": "False",
             "address": "address",
         }
-
+        self.endpoint = self.URL
         citizen_id = self.body.get("citizen_id")
-        requests.delete(f"{self.URL}/registration/{citizen_id}")
+        # remove this registration to avoid already registered case
+        requests.delete(f"{self.URL}/{citizen_id}")
 
     def test_post_registration(self):
         """Send the POST request to create a registration."""
-        endpoint = f"{self.URL}/registration"
-        citizen_id = self.body.get("citizen_id")
-        response = requests.get(f"{self.URL}/registration/{citizen_id}")
-        if response.status_code == 200:
-            response = requests.delete(f"{self.URL}/registration/{citizen_id}")
-        response = requests.post(endpoint, params=self.body)
+        response = requests.post(self.endpoint, params=self.body)
         content = response.json()
         self.assertEqual(201, response.status_code)
         self.assertEqual("registration success!", content["feedback"])
@@ -42,13 +37,8 @@ class RegistrationTest(unittest.TestCase):
 
     def test_post_duplicate_registration(self):
         """Send the POST request with same data 2 times."""
-        endpoint = f"{self.URL}/registration"
-        citizen_id = self.body.get("citizen_id")
-        response = requests.get(f"{self.URL}/registration/{citizen_id}")
-        if response.status_code == 200:
-            response = requests.delete(f"{self.URL}/registration/{citizen_id}")
-        response = requests.post(endpoint, params=self.body)
-        response = requests.post(endpoint, params=self.body)
+        response = requests.post(self.endpoint, params=self.body)
+        response = requests.post(self.endpoint, params=self.body)
         content = response.json()
         self.assertEqual(200, response.status_code)
         self.assertEqual(
@@ -56,33 +46,17 @@ class RegistrationTest(unittest.TestCase):
         )
         self.assertEqual("application/json", response.headers["content-type"])
 
-    def test_post_with_missing_param_address(self):
-        """Send the POST request with 5 query param exclude address."""
-        body = {
-            "citizen_id": "1234567890123",
-            "name": "Chayapol",
-            "surname": "Chaipongsawalee",
-            "birth_date": "2000-05-11",
-            "occupation": "occupation",
-            "phone_number": "0980000000",
-            "is_rick": "False",
-        }
-        endpoint = f"""{self.URL}/registration"""
-        citizen_id = self.body.get("citizen_id")
-        response = requests.get(f"{self.URL}/registration/{citizen_id}")
-        if response.status_code == 200:
-            response = requests.delete(f"{self.URL}/registration/{citizen_id}")
-        response = requests.delete(f"{endpoint}/{citizen_id}")
-        response = requests.post(endpoint, params=body)
+    def test_post_with_missing_param_citizen_id(self):
+        """Send the POST request with missing citizen_id."""
+        self.body.pop("citizen_id")
+        response = requests.post(self.endpoint, params=self.body)
         self.assertEqual(400, response.status_code)
         self.assertEqual("text/html; charset=utf-8", response.headers["content-type"])
 
     def test_post_with_more_than_13_digits_id(self):
         """Send the POST request with invalid citizen id."""
-        body = self.body
-        body["citizen_id"] = "1234567890123123123"
-        endpoint = f"""{self.URL}/registration"""
-        response = requests.post(endpoint, params=self.body)
+        self.body["citizen_id"] = "1234567890123123123"
+        response = requests.post(self.endpoint, params=self.body)
         content = response.json()
         self.assertEqual(200, response.status_code)
         self.assertEqual("registration failed: invalid citizen ID", content["feedback"])
@@ -90,10 +64,8 @@ class RegistrationTest(unittest.TestCase):
 
     def test_post_with_invalid_birth_date(self):
         """Send the POST request with invalid birth date format."""
-        body = self.body
-        body["birth_date"] = "abcaddd"
-        endpoint = f"{self.URL}/registration"
-        response = requests.post(endpoint, params=body)
+        self.body["birth_date"] = "abcaddd"
+        response = requests.post(self.endpoint, params=self.body)
         content = response.json()
         self.assertEqual(200, response.status_code)
         self.assertEqual(
@@ -103,21 +75,26 @@ class RegistrationTest(unittest.TestCase):
 
     def test_post_with_d_m_y_birth_date(self):
         """Send the POST request with dd/mm/yyyy birth date format."""
-        body = self.body
-        body["birth_date"] = "05/11/2000"
-        endpoint = f"{self.URL}/registration"
-        response = requests.post(endpoint, params=body)
+        self.body["birth_date"] = datetime(2000, 11, 5).strftime("%d/%m/%Y")
+        response = requests.post(self.endpoint, params=self.body)
         content = response.json()
         self.assertEqual(201, response.status_code)
         self.assertEqual("registration success!", content["feedback"])
         self.assertEqual("text/html; charset=utf-8", response.headers["content-type"])
 
     def test_post_with_y_m_d_birth_date(self):
+        """Send the POST request with yyyy/mm/dd birth date format."""
+        self.body["birth_date"] = datetime(2000, 11, 5).strftime("%Y/%m/%d")
+        response = requests.post(self.endpoint, params=self.body)
+        content = response.json()
+        self.assertEqual(201, response.status_code)
+        self.assertEqual("registration success!", content["feedback"])
+        self.assertEqual("text/html; charset=utf-8", response.headers["content-type"])
+
+    def test_post_with_m_d_y_birth_date(self):
         """Send the POST request with mm/dd/yyyy birth date format."""
-        body = self.body
-        body["birth_date"] = "11/05/2000"
-        endpoint = f"{self.URL}/registration"
-        response = requests.post(endpoint, params=body)
+        self.body["birth_date"] = datetime(2000, 11, 5).strftime("%m/%d/%Y")
+        response = requests.post(self.endpoint, params=self.body)
         content = response.json()
         self.assertEqual(201, response.status_code)
         self.assertEqual("registration success!", content["feedback"])
@@ -125,10 +102,19 @@ class RegistrationTest(unittest.TestCase):
 
     def test_post_with_m_y_d_birth_date(self):
         """Send the POST request with mm/yyyy/dd birth date."""
-        body = self.body
-        body["birth_date"] = "11/2000/05"
-        endpoint = f"{self.URL}/registration"
-        response = requests.post(endpoint, params=body)
+        self.body["birth_date"] = datetime(2000, 11, 5).strftime("%m/%Y/%d")
+        response = requests.post(self.endpoint, params=self.body)
+        content = response.json()
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(
+            "registration failed: invalid birth date format", content["feedback"]
+        )
+        self.assertEqual("application/json", response.headers["content-type"])
+
+    def test_post_with_d_y_m_birth_date(self):
+        """Send the POST request with dd/yyyy/mm birth date."""
+        self.body["birth_date"] = datetime(2000, 11, 5).strftime("%d/%Y/%m")
+        response = requests.post(self.endpoint, params=self.body)
         content = response.json()
         self.assertEqual(200, response.status_code)
         self.assertEqual(
@@ -138,10 +124,11 @@ class RegistrationTest(unittest.TestCase):
 
     def test_post_with_less_than_12_years_old(self):
         """Send the POST request with less than 12 years old data."""
-        body = self.body
-        body["birth_date"] = "11/05/2020"
-        endpoint = f"{self.URL}/registration"
-        response = requests.post(endpoint, params=body)
+        less_than_12_year_old = datetime.now() - timedelta(
+            weeks=12 * 52
+        )  # 12 years = 52 weeks * 12
+        self.body["birth_date"] = less_than_12_year_old.strftime("%d/%m/%Y")
+        response = requests.post(self.endpoint, params=self.body)
         content = response.json()
         self.assertEqual(200, response.status_code)
         self.assertEqual(
@@ -151,10 +138,9 @@ class RegistrationTest(unittest.TestCase):
 
     def test_post_with_future_date(self):
         """Sent the POST request with future date."""
-        body = self.body
-        body["birth_date"] = "11/05/3000"
-        endpoint = f"{self.URL}/registration"
-        response = requests.post(endpoint, params=body)
+        future_date = datetime.now() + timedelta(days=2)
+        self.body["birth_date"] = future_date.strftime("%d/%m/%Y")
+        response = requests.post(self.endpoint, params=self.body)
         content = response.json()
         self.assertEqual(200, response.status_code)
         self.assertEqual(
@@ -163,20 +149,35 @@ class RegistrationTest(unittest.TestCase):
         self.assertEqual("application/json", response.headers["content-type"])
 
     def test_remove_invalid_registration(self):
-        """Send the DELETE request with invalid citizen id"""
+        """Send the DELETE request with invalid citizen id."""
         citizen_id = self.body.get("citizen_id")
-        endpoint = f"{self.URL}/registration{citizen_id}"
-        response = requests.delete(endpoint, params=self.body)
+        response = requests.delete(f"{self.endpoint}/{citizen_id}", params=self.body)
         self.assertEqual(404, response.status_code)
         self.assertEqual("text/html; charset=utf-8", response.headers["content-type"])
 
     def test_remove_registration(self):
-        """Sent the DELETE request."""
+        """Sent the DELETE request to remove a registration."""
         citizen_id = self.body.get("citizen_id")
-        endpoint = f"{self.URL}/registration"
-        response = requests.post(endpoint, params=self.body)
+        response = requests.post(self.endpoint, params=self.body)
         self.assertEqual(201, response.status_code)
-        endpoint = f"{self.URL}/registration{citizen_id}"
-        response = requests.delete(endpoint, params=self.body)
+        response = requests.delete(f"{self.endpoint}/{citizen_id}", params=self.body)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual("text/html; charset=utf-8", response.headers["content-type"])
+
+    def test_get_registration(self):
+        """Send the GET request to retrive a registration."""
+        citizen_id = self.body.get("citizen_id")
+        response = requests.post(self.endpoint, params=self.body)
+        response = requests.get(f"{self.endpoint}/{citizen_id}", params=self.body)
+        self.assertEqual(200, response.status_code)
+        content = response.json()
+        self.body.update({"vaccine_taken": "[]"})
+        self.assertEqual(self.body, content)
+        self.assertEqual("text/html; charset=utf-8", response.headers["content-type"])
+
+    def test_get_invalid_registration(self):
+        """Send the GET request with invalid citizen id."""
+        citizen_id = "123134567868"
+        response = requests.get(f"{self.endpoint}/{citizen_id}", params=self.body)
         self.assertEqual(404, response.status_code)
         self.assertEqual("text/html; charset=utf-8", response.headers["content-type"])
